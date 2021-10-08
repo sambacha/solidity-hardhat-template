de02081792ee6b162abf11b1b780edf7a7724859
sam bacha <sam@manifoldfinance.com>
2021-10-08 16:21:03 -0700

    fix(hh): config update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,25 +1,40 @@
-import '@typechain/hardhat';
-import 'dotenv/config';
-import { HardhatUserConfig } from 'hardhat/types';
+/**
+* @package solidity-hardhat-template
+* @file hardhat configuration
+* @version 1.0.0
+*/
 
-const HARDHAT_ENABLE_SENTRY = false;
-const mnemonic = 'test test test test test test test test test test test junk';
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
+import { config as dotenvConfig } from "dotenv";
+dotenvConfig({ path: resolve(__dirname, "./.env") });
+
+import { HardhatUserConfig } from "hardhat/types";
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
+const ALCHEMY_KEY = process.env.ALCHEMY_KEY || "";
 
 const config: HardhatUserConfig = {
-  solidity: {
-    version: '0.8.0',
-    settings: {
-      metadata: {
-        bytecodeHash: 'none',
-      },
-      optimizer: {
-        enabled: false,
-        runs: 200,
-        details: {
-          yul: false,
+    solidity: {
+        version: "0.6.12",
+        settings: {
+            metadata: {
+                bytecodeHash: "none",
+            },
+            optimizer: {
+                enabled: false,
+                runs: 200,
+                details: {
+                    yul: false,
+                },
+            },
         },
-      },
     },
- }
+    typechain: {
+        outDir: "typechain",
+        target: "ethers-v5",
+    },
 };
+
 export default config;
