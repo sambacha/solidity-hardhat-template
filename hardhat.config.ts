a328cc06397105afd656086d2876cbfa91345c02
sam bacha <sam@manifoldfinance.com>
2021-10-24 17:42:56 -0700

    build(solc): document solc compiler settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,12 +12,16 @@ const mnemonic = 'test test test test test test test test test test test junk';
 
 const config: HardhatUserConfig = {
   solidity: {
+    
     version: '0.7.4',
     settings: {
       metadata: {
         bytecodeHash: 'none',
       },
       optimizer: {
+     // WARNING: Before version 0.8.6 omitting the 'enabled' key was not equivalent to setting
+     // it to false and would actually disable all the optimizations.
+     // see: https://docs.soliditylang.org/en/latest/using-the-compiler.html#compiler-input-and-output-json-description
         enabled: true,
         runs: 200,
         details: {
