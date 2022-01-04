0c7472b036ad056c33f2067210d05b7d3b18f929
sam bacha <sam@manifoldfinance.com>
2022-01-04 13:47:15 -0800

    fix(hh): configuration (#12)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,42 +11,59 @@ import { HardhatUserConfig } from 'hardhat/types';
 const mnemonic = 'test test test test test test test test test test test junk';
 
 const config: HardhatUserConfig = {
-  solidity: {
-    
-    version: '0.7.4',
+/**
+ * @note
+ * Before version 0.8.6 omitting the 'enabled' key was not equivalent to setting
+ * it to false and would actually disable all the optimizations.
+ * @see: {@link https://docs.soliditylang.org/en/latest/using-the-compiler.html#compiler-input-and-output-json-description}
+ * 
+ */
+
+solidity: {
+    version: '0.8.10',
     settings: {
-      metadata: {
-        bytecodeHash: 'none',
-      },
-      optimizer: {
-     // WARNING: Before version 0.8.6 omitting the 'enabled' key was not equivalent to setting
-     // it to false and would actually disable all the optimizations.
-     // see: https://docs.soliditylang.org/en/latest/using-the-compiler.html#compiler-input-and-output-json-description
-        enabled: true,
-        runs: 200,
-        details: {
-          yul: false,
+        metadata: {
+            bytecodeHash: 'none',
         },
-      },
+        optimizer: {
+            enabled: true,
+            runs: 200,
+            details: {
+                yul: false,
+            },
+        },
+        outputSelection: {
+            "*": {
+                "*": [
+                    "abi",
+                    "evm.bytecode",
+                    "evm.deployedBytecode",
+                    "evm.methodIdentifiers",
+                    "metadata",
+                ],
+                "": ["ast"],
+            },
+        },
     },
-  },
-  typechain: {
-    outDir: 'src/',
-    target: 'ethers-v5',
-  },
+},
+    typechain: {
+        outDir: 'src/',
+        target: 'ethers-v5',
+    },
 };
 
+// @note Compiler output configuration for verifying on Sourceify
 export const defaultSolcOutputSelection = {
-  "*": {
-    "*": [
-      "abi",
-      "evm.bytecode",
-      "evm.deployedBytecode",
-      "evm.methodIdentifiers",
-      "metadata",
-    ],
-    "": ["ast"],
-  },
+    "*": {
+        "*": [
+            "abi",
+            "evm.bytecode",
+            "evm.deployedBytecode",
+            "evm.methodIdentifiers",
+            "metadata",
+        ],
+        "": ["ast"],
+    },
 };
 
 export default config;
