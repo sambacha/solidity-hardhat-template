558bed2adc935fe7c5424a664dacd3b6f9dc3a41
sam bacha <sam@manifoldfinance.com>
2022-03-20 07:56:03 -0700

    feat(hardhat): hardhat config update (#14)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,75 +1,96 @@
 /**
  * @package solidity-hardhat-template
+ * @filename hardhat.config.ts
  * @file hardhat configuration
- * @version 1.1.0
- * @type import('hardhat/config').HardhatUserConfig
+ * @version 1.2.0
  */
+
+import '@nomiclabs/hardhat-etherscan';
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-ethers';
 import '@typechain/hardhat';
 import 'dotenv/config';
 import { HardhatUserConfig } from 'hardhat/types';
 
-const mnemonic = 'test test test test test test test test test test test junk';
+const test_mnemonic = 'test test test test test test test test test test test junk';
 
+/** @type import('hardhat/config').HardhatUserConfig */
 const config: HardhatUserConfig = {
 /**
- * @note
- * Before version 0.8.6 omitting the 'enabled' key was not equivalent to setting
- * it to false and would actually disable all the optimizations.
- * @see: {@link https://docs.soliditylang.org/en/latest/using-the-compiler.html#compiler-input-and-output-json-description}
- * 
- */
-
+* @note
+* Before version 0.8.6 omitting the 'enabled' key was not equivalent to setting
+* it to false and would actually disable all the optimizations.
+* @see: {@link https://docs.soliditylang.org/en/latest/using-the-compiler.html#compiler-input-and-output-json-description}
+*
+*/
 solidity: {
     version: '0.8.10',
     settings: {
-        metadata: {
-            bytecodeHash: 'none',
+      metadata: {
+        bytecodeHash: 'none',
+      },
+      optimizer: {
+        enabled: true,
+        runs: 1_000,
+        details: {
+          yul: false,
         },
-        optimizer: {
-            enabled: true,
-            runs: 200,
-            details: {
-                yul: false,
-            },
+      },
+      outputSelection: {
+        '*': {
+          '*': [
+            'abi',
+            'evm.bytecode',
+            'evm.deployedBytecode',
+            'evm.methodIdentifiers',
+            'metadata',
+          ],
+          '': ['ast'],
         },
-        outputSelection: {
-            "*": {
-                "*": [
-                    "abi",
-                    "evm.bytecode",
-                    "evm.deployedBytecode",
-                    "evm.methodIdentifiers",
-                    "metadata",
-                ],
-                "": ["ast"],
-            },
-        },
+      },
     },
-},
- paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts"
   },
-    typechain: {
-        outDir: 'types/',
-        target: 'ethers-v5',
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: false,
     },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.GOERLI_RPC}`,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  paths: {
+    sources: './contracts',
+    tests: './test',
+    cache: './cache',
+    artifacts: './artifacts',
+  },
+  typechain: {
+    outDir: 'types/',
+    target: 'ethers-v5',
+  },
 };
 
-// @note Compiler output configuration for verifying on Sourceify
+/** @note Compiler output configuration for verifying on Sourceify */
 export const defaultSolcOutputSelection = {
-    "*": {
-        "*": [
-            "abi",
-            "evm.bytecode",
-            "evm.deployedBytecode",
-            "evm.methodIdentifiers",
-            "metadata",
-        ],
-        "": ["ast"],
-    },
+  '*': {
+    '*': [
+      'abi',
+      'evm.bytecode',
+      'evm.deployedBytecode',
+      'evm.methodIdentifiers',
+      'metadata',
+    ],
+    '': ['ast'],
+  },
 };
 
 export default config;
